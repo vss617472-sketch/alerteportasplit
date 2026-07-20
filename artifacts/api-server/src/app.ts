6bcd4bb954105fc012693feb5468c45e74ad9df1
vss617472-sketch <vss617472@gmail.com>
2026-07-20 16:38:58 +0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
 import router from "./routes";
-import { logger } from "./lib/logger";
 
 const app: Express = express();
-
-app.use(
-  (pinoHttp as any)({
-    logger,
-    serializers: {
-      req: (req: any) => {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res: (res: any) => {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  })
-);
 
 app.use(cors());
 app.use(express.json());
